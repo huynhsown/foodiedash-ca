1f68f2f44bf916afcadf6279c7fbe56d0ecd8445
huynhsown <sondanghuynh@gmail.com>
2026-03-12 17:00:51 +0700

    feat: add order item option value & order status history

---
+++ src/main/java/com/ute/foodiedash/domain/order/enums/OrderStatus.java
@@ -0,0 +1,11 @@
+package com.ute.foodiedash.domain.order.enums;
+
+public enum OrderStatus {
+    PENDING,
+    ACCEPTED,
+    PREPARING,
+    READY,
+    DELIVERING,
+    COMPLETED,
+    CANCELLED
+}
