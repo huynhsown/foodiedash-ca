54374143c3708e2f9696aa6d4c7588b5a023dcc1
huynhsown <sondanghuynh@gmail.com>
2026-04-09 23:22:25 +0700

    feat: add lightweight order validation query for review creation

--- src/main/java/com/ute/foodiedash/interfaces/rest/review/ReviewController.java
+++ src/main/java/com/ute/foodiedash/interfaces/rest/review/ReviewController.java
@@ -15,13 +15,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Collections;
@@ -38,7 +32,7 @@ public class ReviewController {
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ReviewResponseDTO> create(
-            @Valid @RequestPart("dto") CreateReviewRequestDTO dto,
+            @Valid @ModelAttribute("dto") CreateReviewRequestDTO dto,
             @RequestPart(value = "images", required = false) List<MultipartFile> images
     ) {
         Long customerId = SecurityContextHelper.getCurrentUserId();
